28954d3dc1f215d42e41e99d16dcb388a343b1da
Akex2 <schointss@gmail.com>
2017-03-19 20:17:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
-#sudo su
-echo "update"
+echo "+++++++++++++++ install update"
 apt-get update
-echo "git"
-apt-get install git
-echo "python-pip"
-apt-get install python-pip
-echo "python-pycurl"
-apt-get install python-pycurl
-echo "python-pyqtgraph"
-apt-get install python-pyqtgraph
-echo "python-qt4"
+
+echo "+++++++++++++++ install git"
+apt-get install -y git
+
+echo "+++++++++++++++ install python-pip"
+apt-get install -y python-pip
+
+echo "+++++++++++++++ install python-pycurl"
+apt-get install -y python-pycurl
+
+echo "+++++++++++++++ install python-pyqtgraph"
+wget http://www.pyqtgraph.org/downloads/0.10.0/pyqtgraph-0.10.0.tar.gz
+tar -xzvf pyqtgraph-0.10.0.tar.gz
+cd pyqtgraph-0.10.0
+python setup.py install
+cd ..
+
+echo "+++++++++++++++ install python-qt4"
 apt-get -y install python-qt4
-#sudo apt-get install xorg
-echo "xinit et unclutter"
-apt-get install xinit unclutter
-echo "cd root"
-cd /root/
-echo "clone"
-git clone https://github.com/Akex2/AkexUI.git
-cd AkexUI/
+
+echo "+++++++++++++++ install websocket-client"
+git clone https://github.com/websocket-client/websocket-client.git
+cd websocket-client/
+python setup.py install
+cd ..
+
+echo "+++++++++++++++ setup screen for BBB"
+bash /opt/scripts/tools/graphics/ti-tilcdc.sh
+
+echo "+++++++++++++++ install numpy"
+apt-get install -y python-numpy
+
+echo "+++++++++++++++ install python-yaml"
+apt-get install -y python-yaml
+
+echo "+++++++++++++++ install xinit et unclutter"
+apt-get install -y xinit unclutter
+
+
 exit 0
